8bfc751ab1ce22a46e8301fd671ccaf2b700afe2
hari <sharikumar1@deloitte.com>
2026-05-20 11:51:31 +0530

    New changes

--- src/test/java/pages/ReservePage.java
+++ src/test/java/pages/ReservePage.java
@@ -7,30 +7,28 @@ import java.util.List;
 
 public class ReservePage extends PageObject {
 
-    public double chooseCheapestFlight() {
+   public double chooseCheapestFlight() {
+        List<WebElementFacade> buttons = findAll("//input[@type='submit']");
+        List<WebElementFacade> priceCells = findAll("//input[@type='submit']/../following-sibling::td[5]");
 
-        double cheapestPrice = Double.MAX_VALUE;
-        int i = 0;
+        double cheapestPrice = Double.POSITIVE_INFINITY;
         WebElementFacade cheapestButton = null;
-        List<WebElementFacade> prices = findAll("//input[@type='submit']/../following-sibling::td[5]");
 
-        for (WebElementFacade price : prices) {
-            double currentPrice = Double.parseDouble(
-                    price.getText().replace("$", "").replace(",", "").trim()
-            );
+        for (int i = 0; i < priceCells.size(); i++) {
+            double currentPrice = parsePrice(priceCells.get(i).getText());
 
             if (currentPrice < cheapestPrice) {
                 cheapestPrice = currentPrice;
-                cheapestButton = find("(//input[@type='submit'])[" + (i + 1) + "]");
+                cheapestButton = buttons.get(i);
             }
-            i++;
         }
 
-        if (cheapestButton == null) {
-            throw new IllegalStateException("No flight rows found.");
-        }
-
-        cheapestButton.click();
+       cheapestButton.click();
         return cheapestPrice;
     }
+
+    private double parsePrice(String priceText) {
+        return Double.parseDouble(priceText.replace("$", "").replace(",", "").trim());
+    }
+
 }
